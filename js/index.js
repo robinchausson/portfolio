b37dbc0590abd60d1eb91565e61f6b5ddc6829b5
calz <75769449+PsykoDiesel@users.noreply.github.com>
2022-06-02 21:12:30 +0200

    - qualité + compétence + travaux

--- js/index.js
+++ js/index.js
@@ -54,17 +54,7 @@ $(document).ready(function() {
     let heightFromTopSect3 = $("#sect3").position().top
     let heightFromTopSect4 = $("#sect4").position().top
 
-    let leftQualite1 = $('#qualite1').position().left
-    let leftQualite2 = $('#qualite2').position().left
-    let topQualite1 = $('#qualite1').position().top + $('#qualite1').innerHeight()/2
-    let topQualite2 = $('#qualite2').position().top + $('#qualite2').innerHeight()/2
-    let topQualite3 = $('#qualite3').position().top + $('#qualite3').innerHeight()/2
-    let leftQualite3 = $('#qualite3').position().left
-    let leftQualite4 = $('#qualite4').position().left
-    let widthQualite = $('#qualite1').innerWidth()
-    let distQualite12 = leftQualite2-leftQualite1-widthQualite
-    let distQualite23 = leftQualite3-leftQualite2-widthQualite
-    let distQualite34 = leftQualite4-leftQualite3-widthQualite
+    let heightFromTopCompetences = $('#sect2 .competences').offset().top
 
     // Code principal
     // Click en dehors
@@ -99,13 +89,12 @@ $(document).ready(function() {
     var lastScrollTop = 0;
     $(window).scroll(function(){
         fixParticules()
+        
         var st = $(this).scrollTop();
         // Lancer les animations des titres
         if (st == 0) {
             $('body > header').css({'border-color':'transparent'})
-            // On reset toutes les animations
-            $('.section-defilement header .fond').css('animation','none')
-            $('.section-defilement header h1').css('animation','none')
+            $('#sect2 .competences-bar > div').css('height','0%')
         }
         // titre section 1
         else if (st > heightFromTopSect2-screenHeight/1.15 && st < heightFromTopSect3-screenHeight/1.15) {
@@ -127,26 +116,13 @@ $(document).ready(function() {
         // Section 1 -> changer la navbar + animation de la section
         if (st < heightFromTopSect2-headerHeight-50) {
             navOnBarreSwitch($('nav li:nth-of-type(1)'))
-            $('#sect2 .qualite').css('animation','none')
-            $('#sect2 .qualite-bar').css('width','0px')
         }
         // Section 2 -> animation des qualités
         else if (st >= heightFromTopSect2-headerHeight-50 && st < heightFromTopSect3-headerHeight-50) {
             navOnBarreSwitch($('nav li:nth-of-type(2)'))
-            $('#qualite1').css('animation','qualite .4s forwards')
-            $('#qualite2').css('animation','qualite .4s .4s forwards')
-            $('#qualite3').css('animation','qualite .4s .8s forwards')
-            $('#qualite4').css('animation','qualite .4s 1.2s forwards')
-            // Barre pour connecter les qualités entre elles
-            setTimeout(() => {
-                $('#qualite-bar1').css({'left':leftQualite1+widthQualite+'px','width':distQualite12+'px','top':topQualite1+'px'})
-            }, 400);
-            setTimeout(() => {
-                $('#qualite-bar2').css({'left':leftQualite2+widthQualite+'px','width':distQualite23+'px','top':topQualite2+'px'})
-            }, 800);
-            setTimeout(() => {
-                $('#qualite-bar3').css({'left':leftQualite3+widthQualite+'px','width':distQualite34+'px','top':topQualite3+'px'})
-            }, 1200);
+            if (st >= heightFromTopCompetences-headerHeight-2/3*screenHeight) {
+                $('#sect2 .competences-bar > div').css('height','100%')
+            }
         }
         else if (st >= heightFromTopSect3-headerHeight-50 && st < heightFromTopSect4-headerHeight-50) {
             navOnBarreSwitch($('nav li:nth-of-type(3)'))
