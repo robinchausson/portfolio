a4c520bc557e611d491cc7b2f9532992f5c000a8
calz <75769449+PsykoDiesel@users.noreply.github.com>
2023-03-11 17:39:43 +0100

    rebrand section about
    ajout de vague, changement de couleur. Effet au scroll sur la photo de profil

--- js/index.js
+++ js/index.js
@@ -23,96 +23,110 @@ $(document).ready(function() {
         }, transitionTime);
     }
     function openMenu() {
-        // On affiche la banniere de transition
-        $('header').css({'background-color':'transparent','box-shadow':'none'})
-        $('#transition').css({'z-index':'9'})
-        startTransition()
+        if (!isMenuOpen && !onMenuTransition) {
+            onMenuTransition = true
 
-        // On anime les barre du menu burger
-        $('#header-menu > div:nth-of-type(2)').css({'width':'0'})
-        setTimeout(() => {
-            $('#header-menu > div:nth-of-type(1)').css({'width':'80%','transform':'translateY(5.9px)rotate(-45deg)'})
-            $('#header-menu > div:nth-of-type(3)').css({'width':'80%','transform':'translateY(-5.9px)rotate(45deg)'})
-        }, 300);
+            // On affiche la banniere de transition
+            $('header').css({'background-color':'transparent','box-shadow':'none'})
+            $('#transition').css({'z-index':'9'})
+            startTransition()
 
-        setTimeout(() => {
-            $('header ul').css({'height': wHeight+'px'})
-            $('header ul li:nth-of-type(1)').css('animation','fadeDown .2s forwards')
-            $('header ul li:nth-of-type(2)').css('animation','fadeDown .2s .15s forwards')
-            $('header ul li:nth-of-type(4)').css('animation','fadeDown .2s .3s forwards')
-            $('header ul li:nth-of-type(5)').css('animation','fadeDown .2s .45s forwards')
-        
+            // On anime les barre du menu burger
+            $('#header-menu > div:nth-of-type(2)').css({'width':'0'})
             setTimeout(() => {
-                $('header ul li').css({'visibility':'visible','animation':'none'})
-            }, 650);
-        }, transitionTime);
+                $('#header-menu > div:nth-of-type(1)').css({'width':'80%','transform':'translateY(5.9px)rotate(-45deg)'})
+                $('#header-menu > div:nth-of-type(3)').css({'width':'80%','transform':'translateY(-5.9px)rotate(45deg)'})
+            }, 300);
+
+            setTimeout(() => {
+                $('header ul').css({'height': wHeight+'px'})
+                $('header ul li:nth-of-type(1)').css('animation','fadeDown .2s forwards')
+                $('header ul li:nth-of-type(2)').css('animation','fadeDown .2s .15s forwards')
+                $('header ul li:nth-of-type(4)').css('animation','fadeDown .2s .3s forwards')
+                $('header ul li:nth-of-type(5)').css('animation','fadeDown .2s .45s forwards')
+            
+                setTimeout(() => {
+                    $('header ul li').css({'visibility':'visible','animation':'none'})
+                    isMenuOpen = true
+                onMenuTransition = false
+
+                }, 650);
+            }, transitionTime);
+        }   
     }
     function closeMenu() {
-        // On anime les barres du menu burger
-        $('header ul li:nth-of-type(5)').css('animation','fadeDown .2s reverse forwards')
-        $('header ul li:nth-of-type(4)').css('animation','fadeDown .2s .15s reverse forwards')
-        $('header ul li:nth-of-type(2)').css('animation','fadeDown .2s .3s reverse forwards')
-        $('header ul li:nth-of-type(1)').css('animation','fadeDown .2s .45s reverse forwards')
+        if (isMenuOpen && !onMenuTransition) {
+            onMenuTransition = true
 
-        $('#header-menu > div:nth-of-type(1)').css({'width':'100%','transform':'rotate(0deg)'})
-        $('#header-menu > div:nth-of-type(3)').css({'width':'100%','transform':'rotate(0deg)'})
-        setTimeout(() => {
-            $('#header-menu > div:nth-of-type(2)').css({'width':'100%'})
-        }, 300);
+            // On anime les barres du menu burger
+            $('header ul li:nth-of-type(5)').css('animation','fadeDown .2s reverse forwards')
+            $('header ul li:nth-of-type(4)').css('animation','fadeDown .2s .15s reverse forwards')
+            $('header ul li:nth-of-type(2)').css('animation','fadeDown .2s .3s reverse forwards')
+            $('header ul li:nth-of-type(1)').css('animation','fadeDown .2s .45s reverse forwards')
 
-        setTimeout(() => {
-            $('header ul li').css({'visibility':'hidden'})
-        
-            // On retire la banniere de transition
-            $('header ul').css({'height': '0'})
-            endTransition()
+            $('#header-menu > div:nth-of-type(1)').css({'width':'100%','transform':'rotate(0deg)'})
+            $('#header-menu > div:nth-of-type(3)').css({'width':'100%','transform':'rotate(0deg)'})
             setTimeout(() => {
-                $('header').css({'background-color':'inherit'})
-                $('#transition').css({'z-index':'100'})
-                $('header ul li').css('animation','none')
-            }, 100);
-        }, 650);
-    }
+                $('#header-menu > div:nth-of-type(2)').css({'width':'100%'})
+            }, 300);
 
-    $('a').hover(function() {
-        $('#mouse-circle').css({'height':'2vw','width':'2vw'}) 
-    },
-    function() {
-        $('#mouse-circle').css({'height':'2.5vw','width':'2.5vw'})
-    })
+            setTimeout(() => {
+                $('header ul li').css({'visibility':'hidden'})
+            
+                // On retire la banniere de transition
+                $('header ul').css({'height': '0'})
+                endTransition()
+                setTimeout(() => {
+                    $('header').css({'background-color':'inherit'})
+                    $('#transition').css({'z-index':'100'})
+                    $('header ul li').css('animation','none')
+
+                    isMenuOpen = false
+                    onMenuTransition = false
+
+                }, 100);
+            }, 650);
+        }
+        
+    }
 
     // Effet de transition sur click de href
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
 
         var href = $(this).attr('href')
-        var coord = $(href).offset().top-headerHeight
+        var coord = $(href).position().top-headerHeight+$(href).children('.top-wave').height()
         // Pour scroller vraiment jusqu'en haut.
         if (href == '#home') {
             coord = 0
         }
         // Transition
-        if (isMenuOpen) {
+        if (isMenuOpen && !onMenuTransition) {
             closeMenu()
+
+            // Scroll
+            setTimeout(() => {
+                $('html, body').animate({
+                    scrollTop: coord
+                }, 10);
+            }, 300);
         }
-        else {
+        else if (!isMenuOpen && !onMenuTransition) {
             startTransition()
             setTimeout(() => {
                 endTransition()
             }, transitionTime);
-        }
 
-        // Scroll
-        setTimeout(() => {
-            $('html, body').animate({
-                scrollTop: coord
-            }, 500);
-        }, 300);
-        
-        
-        
+            // Scroll
+            setTimeout(() => {
+                $('html, body').animate({
+                    scrollTop: coord
+                }, 10);
+            }, 300);
+        }
     });
 
+   
     // Effet sur le header lorsque l'on scroll (pour mieux le voir)
     $(window).on('scroll', function() {
         if ($(window).scrollTop() > 20 && !onTransition) {
@@ -122,32 +136,42 @@ $(document).ready(function() {
             $('header').css('box-shadow', 'none')
         }
 
-        if ($(window).scrollTop()+headerHeight+20 >= $('#about').offset().top) {
-            $('header li a').removeClass('on-text-link')
-            $('.link-about').addClass('on-text-link')
+        // Changer le topic du menu
+        var scrollTopAdjusted = $(window).scrollTop()+headerHeight+wHeight/3
+        
+        // Home
+        if (scrollTopAdjusted < $('#about').offset().top) {
+            if (!$('.home-about').hasClass('on-text-link')) {
+                $('header li a').removeClass('on-text-link')
+                $('.link-home').addClass('on-text-link')
+            }
+        }
+        // About
+        if (scrollTopAdjusted >= $('#about').offset().top && scrollTopAdjusted < $('#work').offset().top) {
+            if (!$('.link-about').hasClass('on-text-link')) {
+                $('header li a').removeClass('on-text-link')
+                $('.link-about').addClass('on-text-link')
+
+                $('.img-pp').css({'max-height':'23vw','max-width':'23vw'})
+                $('.img-pp img').css({'border-color':'inherit'})
+                $('.cercle-int, .cercle-ext').css({'opacity':'.6'})
+            }
         }
         else {
-            $('header li a').removeClass('on-text-link')
-            $('.link-home').addClass('on-text-link')
+            if ($('.img-pp').css('max-height') != '0') {
+                $('.img-pp').css({'max-height':'0','max-width':'0'})
+                $('.img-pp img').css({'border-color':'transparent'})
+                $('.cercle-ext, .cercle-int').css({'opacity':'0'})
+            }
+        }
+        // Work
+        if (scrollTopAdjusted >= $('#work').offset().top) {
+            if (!$('.link-work').hasClass('on-text-link')) {
+                $('header li a').removeClass('on-text-link')
+                $('.link-work').addClass('on-text-link')
+            }
         }
     })
-
-    // Effet sur la souris
-    var mouseX, mouseY;
-    var following = false
-    $(document).mousemove(function(e) {
-        mouseX = e.pageX;
-        mouseY = e.pageY;
-        if (!following && mouseX < wWidth-40) {
-            
-            $('#mouse-circle').css({'transform':'translate3d(calc('+mouseX+'px - 50%) ,calc('+mouseY+'px - 50%),0)'})
-            following = true
-            setTimeout(() => {
-                following = false
-            }, 5);
-        }
-        
-    }).mouseover(); // call the handler immediately
 
     // Set la hauteur de la première section
     $('section:nth-of-type(1)').css('height',wHeight-headerHeight+'px')
@@ -176,23 +200,11 @@ $(document).ready(function() {
 
     // Gérer l'animation du menu sur téléphone
     $('#header-menu').click(function() {
-        if (!onMenuTransition) {
-            if (!isMenuOpen) {
-                openMenu()
-                onMenuTransition = true
-                setTimeout(() => {
-                    isMenuOpen = true
-                    onMenuTransition = false
-                }, 1300);
-            }
-            else {
-                closeMenu()
-                onMenuTransition = true
-                setTimeout(() => {
-                    isMenuOpen = false
-                    onMenuTransition = false
-                }, 1300);
-            }
+        if (!isMenuOpen) {
+            openMenu()
+        }
+        else {
+            closeMenu()
         }
     })
     
